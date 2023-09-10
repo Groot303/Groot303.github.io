633f092b8c29ce9320d23ade90b6d0a3d3330a86
zl <654645595@qq.com>
2023-09-10 21:16:51 +0800

    feat: 更新文档

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -34,38 +34,38 @@ export default defineConfig({
         ],
         sidebar: {
             '/html&css&js': [{
-                    text: 'html',
-                    items: [
-                        { text: '基础知识', link: '/html&css&js/html/基础知识' },
-                        { text: 'html5知识', link: '/html&css&js/html/html5' },
+                text: 'html',
+                items: [
+                    { text: '基础知识', link: '/html&css&js/html/基础知识' },
+                    { text: 'html5知识', link: '/html&css&js/html/html5' },
 
-                    ],
-                    collapsible: true,
-                    collapsed: true
-                },
-                {
-                    text: 'css',
-                    items: [
-                        { text: 'css基础知识', link: '/html&css&js/css/css基础知识' },
-                        { text: 'css3', link: '/html&css&js/css/css3' },
-                    ],
-                    collapsible: true,
-                    collapsed: true
-                },
-                {
-                    text: 'js',
-                    items: [
-                        { text: 'js的基础问题', link: '/html&css&js/JavaScript/js的基础问题' },
-                        { text: 'js的数据类型相关问题', link: '/html&css&js/JavaScript/js的数据类型相关问题' },
-                        { text: 'call、apply、bind', link: '/html&css&js/JavaScript/call、apply、bind' },
-                        { text: '执行上下文', link: '/html&css&js/JavaScript/执行上下文' },
-                        { text: '原型和原型链', link: '/html&css&js/JavaScript/原型和原型链' },
-                        { text: '闭包', link: '/html&css&js/JavaScript/闭包' },
-                        { text: '函数柯里化', link: '/html&css&js/JavaScript/函数柯里化' },
-                    ],
-                    collapsible: true,
-                    collapsed: true
-                },
+                ],
+                collapsible: true,
+                collapsed: true
+            },
+            {
+                text: 'css',
+                items: [
+                    { text: 'css基础知识', link: '/html&css&js/css/css基础知识' },
+                    { text: 'css3', link: '/html&css&js/css/css3' },
+                ],
+                collapsible: true,
+                collapsed: true
+            },
+            {
+                text: 'js',
+                items: [
+                    { text: 'js的基础问题', link: '/html&css&js/JavaScript/js的基础问题' },
+                    { text: 'js的数据类型相关问题', link: '/html&css&js/JavaScript/js的数据类型相关问题' },
+                    { text: 'call、apply、bind', link: '/html&css&js/JavaScript/call、apply、bind' },
+                    { text: '执行上下文', link: '/html&css&js/JavaScript/执行上下文' },
+                    { text: '原型和原型链', link: '/html&css&js/JavaScript/原型和原型链' },
+                    { text: '闭包', link: '/html&css&js/JavaScript/闭包' },
+                    { text: '函数柯里化', link: '/html&css&js/JavaScript/函数柯里化' },
+                ],
+                collapsible: true,
+                collapsed: true
+            },
 
             ],
 
@@ -79,37 +79,37 @@ export default defineConfig({
             }],
 
             '/Vue': [{
-                    text: 'Vue基础',
-                    items: [
-                        { text: 'vue2基础知识', link: '/Vue基础/vue2基础知识' },
-                        { text: 'vue3基础知识', link: '/Vue基础/vue3基础' },
-                        { text: 'vue3和vue2的区别', link: '/Vue基础/vue3和vue2的区别' },
-                        { text: 'MVC、MVP、MVVM', link: '/Vue基础/MVC、MVP、MVVM' },
+                text: 'Vue基础',
+                items: [
+                    { text: 'vue2基础知识', link: '/Vue基础/vue2基础知识' },
+                    { text: 'vue3基础知识', link: '/Vue基础/vue3基础' },
+                    { text: 'vue3和vue2的区别', link: '/Vue基础/vue3和vue2的区别' },
+                    { text: 'MVC、MVP、MVVM', link: '/Vue基础/MVC、MVP、MVVM' },
 
-                    ],
-                    collapsible: true,
-                    collapsed: true
-                },
-                {
-                    text: 'Vue核心插件',
-                    items: [
-                        { text: 'vuex基础', link: '/Vue核心插件/vuex基础' },
-                        { text: 'vue-router基础', link: '/Vue核心插件/vue-router基础' },
-                        { text: '前端路由', link: '/Vue核心插件/前端路由' },
-                    ],
-                    collapsible: true,
-                    collapsed: true
-                },
-                {
-                    text: 'Vue原理',
-                    items: [
-                        { text: 'vue源码', link: '/Vue原理/vue源码' },
-                        { text: 'vue的双向数据绑定原理', link: '/Vue原理/vue的双向数据绑定原理' },
-                        { text: 'vue3的数据响应式原理', link: '/Vue原理/vue3的数据响应式原理' },
-                    ],
-                    collapsible: true,
-                    collapsed: true
-                },
+                ],
+                collapsible: true,
+                collapsed: true
+            },
+            {
+                text: 'Vue核心插件',
+                items: [
+                    { text: 'vuex基础', link: '/Vue核心插件/vuex基础' },
+                    { text: 'vue-router基础', link: '/Vue核心插件/vue-router基础' },
+                    { text: '前端路由', link: '/Vue核心插件/前端路由' },
+                ],
+                collapsible: true,
+                collapsed: true
+            },
+            {
+                text: 'Vue原理',
+                items: [
+                    { text: 'vue源码', link: '/Vue原理/vue源码' },
+                    { text: 'vue的双向数据绑定原理', link: '/Vue原理/vue的双向数据绑定原理' },
+                    { text: 'vue3的数据响应式原理', link: '/Vue原理/vue3的数据响应式原理' },
+                ],
+                collapsible: true,
+                collapsed: true
+            },
 
             ],
 
@@ -165,11 +165,12 @@ export default defineConfig({
                 items: [
                     { text: 'jwt', link: '/project/宿舍系统/jwt' },
                     { text: '刷新页面vuex状态丢失', link: '/project/宿舍系统/刷新页面vuex状态丢失' },
+                    { text: '路由懒加载的原理', link: '/project/宿舍系统/路由懒加载的原理' },
 
                 ],
                 // collapsible: true,
                 collapsed: true
-            }, 
+            },
             {
                 text: '其他',
                 items: [
@@ -179,7 +180,7 @@ export default defineConfig({
                 ],
                 // collapsible: true,
                 collapsed: true
-            }, ],
+            },],
             '/reprint': [{
                 text: 'css',
                 items: [
--- docs/project/宿舍系统/刷新页面vuex状态丢失.md
+++ docs/project/宿舍系统/刷新页面vuex状态丢失.md
@@ -17,13 +17,20 @@ tags:
 
 :one: **利用localStorage/sessionStorage本地存储一份**
 
+具体实现，在通过mutation修改state数据的同时调用localStorage.setIten()方法进行数据存储，在页面加载（created钩子）的时候再通过localStorage.getItem()将数据取出放回到vuex
+
 - 这种方法解决了问题，但是如果用户手动删除了浏览器的缓存，也会清空。
+- 不断触发localStorage.setItem()方法对性能不是特别友好，而且一直将数据同步到localStorage中似乎就没必要再用vuex做状态管理，直接用localStorage即可
 
 :two: **刷新后重新发送请求**
 
 - 这种方式减少了本地存储的使用，但是增加了请求的次数，遇到数据量大的时候一般使用这种方法
 
-:three: **使用插件 vuex-persistedstate**
+:three: **基于computed计算属性**
+
+- 因为计算属性只有在相关响应式依赖发生改变时才会重新求值，计算属性的结果是会被储存的，
+
+:four: **使用插件 vuex-persistedstate**
 
 ```js
 import Vue from 'vue'
@@ -49,3 +56,6 @@ plugins: [createPersistedState({
 
 export default store;
 ```
+
+- 在store初始化后，vuex-persistedstate插件会监听每一个mutation，在mutation执行后将整个store的状态保存到本地，例如使用localStorage。当页面刷新时，vuex-persistedstate插件会在store初始化前先读取本地存储的状态，然后再将读取到的状态合并到store中。
+
